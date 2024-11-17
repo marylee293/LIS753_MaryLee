89b99fadafbce24f26fb18d8e7826d85cb87d8a5
marylee293 <94leem@gmail.com>
2024-11-17 17:28:16 -0600

    Commit exercise09.html and script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.getElementById("page-title").style.fontSize="75px";
+
+document.getElementById("page-title").style.color="red";
+
+function myFunction() {
+   var element = document.body;
+   element.classList.toggle("dark-mode");
+}
